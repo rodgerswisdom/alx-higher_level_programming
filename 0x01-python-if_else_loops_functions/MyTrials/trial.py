4160dc067b13f3b7a2e2de1d4b357c1e39d04fa8
rodgerswisdom <trulyhawona@gmail.com>
2024-01-16 23:22:05 +0300

    trial

--- 0x01-python-if_else_loops_functions/MyTrials/trial.py
+++ 0x01-python-if_else_loops_functions/MyTrials/trial.py
@@ -1,6 +1,6 @@
-ascii_z = ord('z')
+ascii_z = ord('z') #ord finds acsii value of z
 
 for i in range(26):
-    print(chr(ascii_z-i), end='')
+    print(chr(ascii_z-i), end='') #chr converts it back to alphabet
 print()
 
