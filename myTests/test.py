ccb779d674949e3cd1d405e7af6b9ebc5c1071aa
rodgerswisdom <trulyhawona@gmail.com>
2024-03-07 23:54:33 +0300

    setup

--- myTests/test.py
+++ myTests/test.py
@@ -5,20 +5,20 @@ from Code.my_calculations import Calculations
 
 class TestCalculations(unittest.TestCase):
 
+
+	def setUp(self):
+		self.calculation = Calculations(8,2)
+
 	def test_sum(self):
-		calculation = Calculations(8,2)
 		self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong')
 		
 	def test_difference(self):
-		calculation = Calculations(8,2)
 		self.assertEqual(calculation.get_difference(), 6, 'The difference is wrong')
 		
 	def test_product(self):
-		calculation = Calculations(8,2)
 		self.assertEqual(calculation.get_product(), 16, 'The product is wrong')
 		
 	def test_quotient(self):
-		calculation = Calculations(8,2)
 		self.assertEqual(calculation.get_quotient(), 4, 'The sum is wrong')
 		
 if __name__== '__main__':
