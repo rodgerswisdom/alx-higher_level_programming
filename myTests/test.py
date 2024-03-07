8c69132a4b51ff2823b7391aae49782b683adfec
rodgerswisdom <trulyhawona@gmail.com>
2024-03-08 00:00:55 +0300

    calcualtions using self coz instance

--- myTests/test.py
+++ myTests/test.py
@@ -10,16 +10,16 @@ class TestCalculations(unittest.TestCase):
 		self.calculation = Calculations(8,2)
 
 	def test_sum(self):
-		self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong')
+		self.assertEqual(self.calculation.get_sum(), 10, 'The sum is wrong')
 		
 	def test_difference(self):
-		self.assertEqual(calculation.get_difference(), 6, 'The difference is wrong')
+		self.assertEqual(self.calculation.get_difference(), 6, 'The difference is wrong')
 		
 	def test_product(self):
-		self.assertEqual(calculation.get_product(), 16, 'The product is wrong')
+		self.assertEqual(self.calculation.get_product(), 16, 'The product is wrong')
 		
 	def test_quotient(self):
-		self.assertEqual(calculation.get_quotient(), 4, 'The sum is wrong')
+		self.assertEqual(self.calculation.get_quotient(), 4, 'The sum is wrong')
 		
 if __name__== '__main__':
 	unittest.main()
