f29c00cbb39f035a9d3dd15387ed2da54598f404
rodgerswisdom <trulyhawona@gmail.com>
2024-03-07 23:41:40 +0300

    test.py

--- myTests/test.py
+++ myTests/test.py
@@ -9,5 +9,17 @@ class TestCalculations(unittest.TestCase):
 		calculation = Calculations(8,2)
 		self.assertEqual(calculation.get_sum(), 10, 'The sum is wrong')
 		
+	def test_difference(self):
+		calculation = Calculations(8,2)
+		self.assertEqual(calculation.get_difference(), 6, 'The difference is wrong')
+		
+	def test_product(self):
+		calculation = Calculations(8,2)
+		self.assertEqual(calculation.get_product(), 16, 'The product is wrong')
+		
+	def test_quotient(self):
+		calculation = Calculations(8,2)
+		self.assertEqual(calculation.get_quotient(), 4, 'The sum is wrong')
+		
 if __name__== '__main__':
 	unittest.main()
