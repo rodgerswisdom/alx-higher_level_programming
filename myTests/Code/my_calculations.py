ca483eef409d507ecd0bb8d32e13d11aeb9fbca6
rodgerswisdom <trulyhawona@gmail.com>
2024-03-07 23:23:39 +0300

    my_calculations.py

---
+++ myTests/Code/my_calculations.py
@@ -0,0 +1,16 @@
+class Calculations:
+	def __init__(self, a, b):
+		self.a = a
+		self.b = b
+		
+	def get_sum(self):
+		return self.a + self.b
+		
+	def get_difference(self):
+		return self.a - self.b
+		
+	def get_product(self):
+		return self.a * self.b
+		
+	def get_quotient(self):
+		return self.a / self.b
