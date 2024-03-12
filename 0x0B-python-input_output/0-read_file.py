324c38e50ec286c4af93ca1f3846c4c5d5f894b9
rodgerswisdom <trulyhawona@gmail.com>
2024-03-12 11:06:24 +0300

    input

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,15 @@
 #!usr/bin/Python3
+"""
+    Defines a python module
+"""
 
-with open('textfile', encoding="utf-8") as file:
-    read_data = file.read()
+def read_file(filename=""):
+"""
+    read_file function
+    reads the UTF-8 format
+"""
+    with open(filename, mode='r', encoding="utf-8") as file:
+	    read_data = file.read()
+	    print(read_data, end='')
 
-file.closed
+    file.closed
