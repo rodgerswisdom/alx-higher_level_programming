6fecbea3f0654a1b477c92637795528213ec04fd
rodgerswisdom <trulyhawona@gmail.com>
2024-07-30 23:18:58 +0300

    102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that was a fun effort in breaking to http protocols on holberton servers
-curl -s -L -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
